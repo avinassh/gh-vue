22d26ccac314f89c88ab97313ed3bb247267102b
avi <hi@avi.im>
2017-03-18 12:00:19 +0530

    Change component name to Github

---
+++ src/components/Github.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <h2>{{ tagline }}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'github',
+  data () {
+    return {
+      title: 'Github Vue',
+      tagline: 'Simple Vue JS app to display your Github Commits'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
