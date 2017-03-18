35a5e6c8f7d2c27b9dd8432316e8808c48f8cb75
avi <hi@avi.im>
2017-03-18 16:19:18 +0530

    Change color for anchor tag

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,28 @@
 </template>
 
 <script>
-import Hello from './components/Hello'
-import Github from './components/Github'
+  import Hello from './components/Hello'
+  import Github from './components/Github'
 
-export default {
-  name: 'app',
-  components: {
-    Hello,
-    Github
+  export default {
+    name: 'app',
+    components: {
+      Hello,
+      Github
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  a {
+    color: #097af1;
+  }
 </style>
